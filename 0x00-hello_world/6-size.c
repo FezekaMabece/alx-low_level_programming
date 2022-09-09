76a93a5d3a45f09be27111fc4b8500eb4c60f1cb
FezekaMabece <fmabece@gmail.com>
2022-09-09 16:23:52 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("%lu\n", sizeof(char));
-        printf("%lu\n", sizeof(int));
-        printf("%lu\n" sizeof(long int));
-        printf("%lu\n", sizeof(long long  int));
-        printf("%lu", sizeof(float));
+printf("sizeof char is %zu\n", sizeof(char));
+printf("sizeof int is %zu\n", sizeof(int));
+printf("sizeof long int is %zu\n", sizeof(long int));
+printf("sizeof long long int is %zu\n", sizeof(long long int));
+printf("sizeof float is %zu\n", sizeof(float));
 return(o);
 }
