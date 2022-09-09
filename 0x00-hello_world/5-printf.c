2cdb5cb975045fa0ad41970857f4f91a192ed467
FezekaMabece <fmabece@gmail.com>
2022-09-09 15:56:32 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Prints "with proper grammar,but the outcome is a piece of art"
  *
  * Return 0 (Success)
  */
 int main(void)
-/* print exxact str */
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
