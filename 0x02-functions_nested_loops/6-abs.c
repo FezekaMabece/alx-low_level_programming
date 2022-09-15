7fea8a1922864f87b9ccb45f4a21dc04a302b67a
FezekaMabece <fmabece@gmail.com>
2022-09-14 18:12:11 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - a function that computes the absolute value of an integer
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < )
+	{ 
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
