7b1f419560fdeaca05d44c551c8043e9aa1f67a1
FezekaMabece <fmabece@gmail.com>
2022-09-26 11:36:38 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,9 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i
+unsigned int i;
+unsigned char *memory = s, value = c;
 for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-return (s);
+memory[i] = value;
+return (memory);
 }
