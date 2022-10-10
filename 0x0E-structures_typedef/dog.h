1749596ba657db5a3b47e219e4f286d99d12d932
FezekaMabece <fmabece@gmail.com>
2022-10-10 04:29:24 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct_dog - Dog attributes
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*
+*/
+struct dog
+{
+char *name
+float age;
+char *owner;
+};
+/**
+* dog_t - typedef dog
+*/
+typedef struct dog dog_t;
+
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
