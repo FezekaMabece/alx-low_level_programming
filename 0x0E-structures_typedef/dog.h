ced346b626c2def5304867787e97d39e9e5ded9c
FezekaMabece <fmabece@gmail.com>
2022-10-11 04:58:22 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG
+#define DOG
 
 /**
 * struct_dog - Dog attributes
@@ -14,15 +14,9 @@ char *name;
 float age;
 char *owner;
 };
-
-/**
-* dog_t - typedef dog
-*/
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age. char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
