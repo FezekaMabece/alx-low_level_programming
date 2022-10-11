6afe46d636208ec8026194f8890de0384ee82b37
FezekaMabece <fmabece@gmail.com>
2022-10-11 04:30:56 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
-*main - prints the name of the file it was compiled
+* main - prints the name of the file it was compiled
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-_putchar("%s\n", __FILE__);
-return (0);i
+printf("%s\n", __FILE__);
+return (0);
 }
